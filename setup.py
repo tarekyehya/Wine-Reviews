9a5da4b72f71513fa63d08305915435b4e579979
tarekyehya <tarek_500@yahoo.com>
2023-08-21 02:19:57 +0200

    project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a regression task, predict the points depend on some text and non-text features with several approches',
+    author='Tarek yahia',
+    license='MIT',
+)
